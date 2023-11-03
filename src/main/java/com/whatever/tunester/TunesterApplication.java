43225db5083f4bb043f923ad04c12db7d8afe317
Oleg Markelov <o.markelov@g.nsu.ru>
2023-11-03 13:36:52 +0700

    Initial commit

---
+++ src/main/java/com/whatever/tunester/TunesterApplication.java
@@ -0,0 +1,11 @@
+package com.whatever.tunester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunesterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TunesterApplication.class, args);
+    }
+}
